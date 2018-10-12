2121c36fc4709c56402bcdafdad2414d2b3f09b3
Md Zishan <m.zishan007@gmail.com>
2018-10-12 13:02:56 +0530

    my project setup completed successfully

---
+++ shopingbackend/src/main/java/com/abc/shopingbackend/App.java
@@ -0,0 +1,13 @@
+package com.abc.shopingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
