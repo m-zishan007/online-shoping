78989ec37a88f5361f17d172083675d2d85b39a5
Md Zishan <m.zishan007@gmail.com>
2018-10-13 23:05:28 +0530

    Active menu problem solved

--- onlineshoping/src/main/java/com/abc/onlineshoping/controller/PageController.java
+++ onlineshoping/src/main/java/com/abc/onlineshoping/controller/PageController.java
@@ -1,7 +1,6 @@
 package com.abc.onlineshoping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,10 +9,26 @@ public class PageController {
 	@RequestMapping(value = {"/", "/home", "index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "welcome to spring mvc");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 	
+	@RequestMapping(value = {"/about"})
+	public ModelAndView about() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value = {"/contact"})
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
 /*	@RequestMapping(value="/test")
 	public ModelAndView test(@RequestParam(value="greeting", required=false) String greeting) {
 		
@@ -25,7 +40,7 @@ public class PageController {
 		return mv;
 	}*/
 	
-	@RequestMapping(value="/test/{greeting}")
+/*	@RequestMapping(value="/test/{greeting}")
 	public ModelAndView test(@PathVariable("greeting") String greeting) {
 		
 		if(greeting == null) {
@@ -34,5 +49,5 @@ public class PageController {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", greeting);
 		return mv;
-	}
+	}*/
 }
